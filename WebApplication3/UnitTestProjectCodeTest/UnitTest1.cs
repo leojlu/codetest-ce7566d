4110033a4e96b0fbe3f4648ccb5d60f71ae8c7a5
Leo Lu <leo@sonrnews.com>
2016-08-10 17:29:43 -0700

    code test by leolu

---
+++ WebApplication3/UnitTestProjectCodeTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication3.Controllers;
+using System.Collections.Generic;
+
+namespace UnitTestProjectCodeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new CodeTestController();
+            List<long> expectedResult = new List<long>();
+            expectedResult.Add(19);
+            expectedResult.Add(28);
+            expectedResult.Add(37);
+            expectedResult.Add(46);
+            expectedResult.Add(55);
+            expectedResult.Add(64);
+            expectedResult.Add(73);
+            expectedResult.Add(82);
+            expectedResult.Add(91);
+            List<long> result = controller.Get("100");
+            Assert.AreEqual(expectedResult.Count, result.Count);
+            for(int i = 0; i < expectedResult.Count; i++)
+                Assert.AreEqual(expectedResult[i], result[i]);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var controller = new CodeTestController();
+            List<long> expectedResult = new List<long>();
+            List<long> result = controller.Get("0");
+            Assert.AreEqual(expectedResult.Count, result.Count);
+          
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var controller = new CodeTestController();
+            List<long> expectedResult = new List<long>();
+            expectedResult.Add(19);
+            List<long> result = controller.Get("19");
+            Assert.AreEqual(expectedResult.Count, result.Count);
+            for (int i = 0; i < expectedResult.Count; i++)
+                Assert.AreEqual(expectedResult[i], result[i]);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var controller = new CodeTestController();
+            List<long> expectedResult = new List<long>();
+            List<long> result = controller.Get("ACD42");
+            Assert.AreEqual(expectedResult.Count, result.Count);
+
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Controllers/CodeTestController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class CodeTestController : ApiController
+    {
+        [System.Web.Http.RouteAttribute("api/codetest")]
+        public List<long> Get(string input)
+        {
+            List<long> result = new List<long>();
+            if(!string.IsNullOrEmpty(input))
+            {
+                int n;
+                bool isNumeric = int.TryParse(input, out n);
+                if(isNumeric) //if it's numeric, loop through every int from 0 to N
+                {
+                    for (int i = 0; i <= n; i++)
+                    {
+                        List<long> temp = Get10Substring(i.ToString());
+                        foreach(long item in temp)
+                        {
+                            if (!result.Contains(item))
+                            {
+                                result.Add(item);
+                            }
+                        }
+                    }
+                }
+                else // if it's not numeric, only check the string itself.
+                {
+                    result = Get10Substring(input);
+                }
+                
+            }
+            return result;
+        }
+
+        private List<long> Get10Substring(string input)
+        {
+            List<long> result = new List<long>();
+            for(int i =0; i < input.Length; i++)
+            {
+                int sum = 0;
+                int j = i;
+                while(sum < 10 && j < input.Length) //continue until sum is equal or greater 10
+                {
+                    int n;
+                    bool isNumeric = int.TryParse(input.Substring(j, 1), out n);
+                    if (!isNumeric)
+                        break;
+                    sum = sum + n;
+                    j++;
+                }
+                if (sum == 10) //add string to result
+                {
+                    result.Add(Convert.ToInt64(input.Substring(i, j - i)));
+                }
+            }
+            return result;
+        }
+
+       
+    }
+}
